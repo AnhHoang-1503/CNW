9bfbf2de4cf78cf7297bfb712c9ac1525584aa8d
Vũ Trí Anh Hoàng <vutrianhhoang@gmail.com>
2023-12-20 07:33:51 +0700

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -20,11 +20,6 @@ const router = createRouter({
             component: () => import("../views/MyCourseView.vue"),
         },
         {
-            path: "/profile",
-            name: "profile",
-            component: () => import("../views/ProfileView.vue"),
-        },
-        {
             path: "/courses/:id",
             name: "course",
             component: () => import("../views/CourseDetailView.vue"),
